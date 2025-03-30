366c400524b54a7bec84ea49f5ad805e94e2dba1
Diego Ruggeri (CW) <druggeri@paypal.com>
2025-03-30 12:45:22 -0300

    Add thumbs and metadata

--- src/sub2md.py
+++ src/sub2md.py
@@ -1,7 +1,6 @@
 import json
-import sys
-import re
 import os
+import sys
 from jsonpath_ng import jsonpath, parse
 
 print(sys.argv)
@@ -14,25 +13,18 @@ for (in_path, _, in_filenames) in os.walk(input):
         id = root.split('.')[0]
         if('json' not in ext): continue
 
-        date, title = in_path.split('/')[-2:]
-        print(date, title, id)
-
+        date, title = in_path.split('/')[-2:].replace('_', ' ')
         with open(os.path.join(in_path, in_filename)) as fin:
             sub = json.load(fin)
 
-        output_path = os.path.join(output, date, title.replace('_', ' '))
-        if not os.path.exists(output_path):
-            os.makedirs(output_path)
+        out_filename = os.path.join(output, id + '.md')
+        img_filename = id + '.webp'
 
-        out_filename = os.path.join(output_path, id + '.md')
-        print(out_filename)
+        header = f'# {title}\n\n'
+        f'[![{title}]({img_filename})](https://www.youtube.com/watch?v={id})'
 
-        with open(out_filename, 'w') as fout:
-            fout.write(f'# {title}\n\n')
-            fout.write(f'<iframe width="560" height="315" src="http://www.youtube.com/embed/{id}" ' +
-                    'title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; ' +
-                    'clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ' +
-                    'referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>\n\n')
+        with open(out_filename, 'w') as f:
+            f.write(header)
             for match in parse('$.events[*].segs').find(sub):
-                fout.write(''.join([seg['utf8'] for seg in match.value])) 
+                f.write(''.join([seg['utf8'] for seg in match.value])) 
 

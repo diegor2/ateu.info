6c8fc754bbd1ca1253a355c969e92e52d47c58dc
Diego Ruggeri (CW) <druggeri@paypal.com>
2025-06-04 23:05:03 -0300

    Retry downloading missing files

--- src/pick.py
+++ src/pick.py
@@ -1,11 +1,28 @@
 import os
 import random
+import sys
+
+# defaults
+skip_existing = True
+only_existing = False
+
+if sys.argv[1] == '-n':
+    skip_existing = True
+elif sys.argv[1] == '-x':
+    only_existing = True
+elif sys.argv[1] == '-a':
+    pass
 
 with open('index.db') as f:
     ids = f.read().splitlines()
-
 locals = os.listdir('database')
-valid = [id for id in ids if id not in locals]
+
+if skip_existing:
+    valid = [id for id in ids if id not in locals]
+elif only_existing:
+    valid = locals
+else:
+    valid = ids
 
 chosen = random.choice(valid)
 print(chosen)
